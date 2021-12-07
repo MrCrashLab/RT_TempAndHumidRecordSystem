d228af8ce3c14df6f28a962f91b613acade36467
MrCrashLab <m.dudarev2002@mail.ru>
2021-12-07 17:14:13 +0300

    Add console worker and functional for microcontroller

---
+++ src/main/java/ru/MrCrashLab/tempAndHumidRecordSystem/ConsoleWorker.java
@@ -0,0 +1,79 @@
+package ru.MrCrashLab.tempAndHumidRecordSystem;
+
+import ru.MrCrashLab.tempAndHumidRecordSystem.Controllers.MicroController;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsoleWorker {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void printMicroControllerList(List<MicroController> microControllerList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("ID\t\tControllerID\tCurrentTemp\t\tCurrentHumid\n");
+        for (int i = 0; i < microControllerList.size(); i++) {
+            stringBuilder.append('[');
+            stringBuilder.append(i);
+            stringBuilder.append("] \t");
+            stringBuilder.append('[');
+            stringBuilder.append(microControllerList.get(i).getID());
+            stringBuilder.append("] \t");
+            if (microControllerList.get(i).getID() < 100000000)
+                stringBuilder.append("\t");
+            stringBuilder.append(microControllerList.get(i).getTempSensor().getData());
+            stringBuilder.append("\t\t\t");
+            if (microControllerList.get(i).getTempSensor().getData() < 10 && microControllerList.get(i).getTempSensor().getData() >= 0)
+                stringBuilder.append("\t");
+            stringBuilder.append(microControllerList.get(i).getHumidSensor().getData());
+            stringBuilder.append("\t");
+            stringBuilder.append('\n');
+        }
+        System.out.println(stringBuilder);
+    }
+
+    public void enterIdForChooseController(List<MicroController> microControllerList) {
+        int id;
+        int mode;
+        float temp;
+        float humid;
+        while (true) {
+            System.out.print("Введите ID контроллера(первая колонка), в которой вы хотите передать значения: ");
+            id = scanner.nextInt();
+            if (id >= microControllerList.size() || id < 0) {
+                System.out.println("\u001B[91mВы ввели некоректное значение!\u001B[0m");
+            } else {
+                break;
+            }
+        }
+        while (true) {
+            System.out.print("Выберите какое значение вы хотите изменить(1 - температура, 2 - влажность): ");
+            mode = scanner.nextInt();
+            if (mode != 1 && mode != 2) {
+                System.out.println("\u001B[91mВы ввели некоректное значение!\u001B[0m");
+            } else {
+                break;
+            }
+        }
+        while (true) {
+            if (mode == 1) {
+                System.out.print("Введите новое значение температуры (Диапазон от -20 до +60): ");
+                temp = scanner.nextInt();
+                if (temp < -20 || temp > 60) {
+                    System.out.println("\u001B[91mВы ввели некоректное значение!\u001B[0m");
+                } else {
+                    microControllerList.get(id).getTempSensor().setData(temp);
+                    break;
+                }
+            } else if (mode == 2) {
+                System.out.print("Введите новое значение влажности (Диапазон от 0 до 99): ");
+                humid = scanner.nextInt();
+                if (humid < 0 || humid > 99) {
+                    System.out.println("\u001B[91mВы ввели некоректное значение!\u001B[0m");
+                } else {
+                    microControllerList.get(id).getHumidSensor().setData(humid);
+                    break;
+                }
+            }
+        }
+    }
+}
--- src/main/java/ru/MrCrashLab/tempAndHumidRecordSystem/Controllers/MicroController.java
+++ src/main/java/ru/MrCrashLab/tempAndHumidRecordSystem/Controllers/MicroController.java
@@ -1,9 +1,66 @@
 package ru.MrCrashLab.tempAndHumidRecordSystem.Controllers;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import ru.MrCrashLab.tempAndHumidRecordSystem.Sensors.HumidSensor;
+import ru.MrCrashLab.tempAndHumidRecordSystem.Sensors.SensorInterface;
 import ru.MrCrashLab.tempAndHumidRecordSystem.Sensors.TempSensor;
 
-public class MicroController{
-    private HumidSensor humidSensor;
-    private TempSensor tempSensor;
+import java.util.Map;
+
+public class MicroController {
+    private final int ID; //уникальный номер микроконтроллера
+    private final int TIME_TO_PUBLISH; //время через которое публикуются значения в топике
+    private final HumidSensor humidSensor;
+    private final String controllerName;
+    private final TempSensor tempSensor;
+    private final Map<String, SensorInterface> sensors;
+
+    public MicroController(int timeToPublish) {
+        ID = this.hashCode();
+        TIME_TO_PUBLISH = timeToPublish;
+        humidSensor = new HumidSensor(0);
+        tempSensor = new TempSensor(0);
+        sensors = Map.of("temperature", tempSensor,
+                "humidity", humidSensor);
+        controllerName = "Controller_" + ID + '/';
+    }
+
+    public void publicData() {
+        String topicName;
+        try {
+            MqttClient client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
+            client.connect();
+            for (Map.Entry<String, SensorInterface> entry : sensors.entrySet()) {
+                topicName = controllerName + entry.getKey();
+                MqttMessage message = new MqttMessage();
+                message.setPayload(Double.toString(entry.getValue().getData()).getBytes());
+                client.publish(topicName, message);
+                System.out.println(message);
+            }
+            Thread.sleep(TIME_TO_PUBLISH);
+            client.disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HumidSensor getHumidSensor() {
+        return humidSensor;
+    }
+
+    public TempSensor getTempSensor() {
+        return tempSensor;
+    }
+
+    public Map<String, SensorInterface> getSensors() {
+        return sensors;
+    }
+
+    public int getID() {
+        return ID;
+    }
 }
